c920c20356e6a7516894fabaa71003ae059748a7
fuckseer <ksusha.magic.96@gmail.com>
2023-07-16 18:58:57 +0500

    Added index.html

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from AddData import prepare_df
 import pandas as pd
+from flask import Flask, render_template
 
+app = Flask(__name__, template_folder='templates')
 
-
+@app.route('/')
+def index():
+     return render_template('index.html')
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-     prepare_df()
+     app.run(debug=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
